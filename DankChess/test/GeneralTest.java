f936d193fb637074bf451d69f6ce60b735c71eb4
ToastyTad <toastytadgit@gmail.com>
2017-11-16 23:05:38 -0500

    Added fxml and test files

---
+++ DankChess/test/GeneralTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GeneralTest
+{
+    @Test
+    public void testTest()
+    {
+        assertTrue(true);
+    }
+}
