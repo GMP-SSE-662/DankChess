230ea77ba88689eda3e5f8b6ded1c4b1b0ced507
SeditiousSnake <tyson31088@gmail.com>
2017-11-18 17:09:47 -0500

    Added Location class, fleshed out ChessPiece class a bit

--- DankChess/src/chess_game/ChessPiece.java
+++ DankChess/src/chess_game/ChessPiece.java
@@ -1,4 +1,18 @@
 package chess_game;
 
+import java.util.ArrayList;
+
 public abstract class ChessPiece {
+    private Location location;
+
+    public Location getLocation(){
+        return location;
+    }
+
+    public void setLocation(int row, int column){
+        location = new Location(row, column);
+    }
+
+    public abstract ArrayList<Location> getValidMoves();
+
 }
---
+++ DankChess/src/chess_game/Location.java
@@ -0,0 +1,19 @@
+package chess_game;
+
+public class Location {
+    private int row;
+    private int column;
+
+    public Location(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getColumn(){
+        return column;
+    }
+}
